e2a7f98c1ab821c8bd4262da1e91ba4f6b4928be
Miguel Angel Tomairo <migueltomairo@multiplica.com>
2019-12-26 20:49:16 -0500

    Initial Commit

---
+++ button-ibdesignable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  button-ibdesignable
+//
+//  Created by miguel tomairo on 12/26/19.
+//  Copyright © 2019 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
